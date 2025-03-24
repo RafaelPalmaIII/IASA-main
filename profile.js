16c34d9b779f5a04960e82d4daf9006ffdfa86e3
RafaelPalmaIII <rafaeliiipalma@gmail.com>
2025-03-24 20:17:49 +0800

    Ikatulo

--- profile.js
+++ profile.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
-import { getFirestore , getDoc , setDoc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAnwc89hsjVXilrhYH5z3dZpKsQDwB29UA",
@@ -10,7 +10,6 @@ const firebaseConfig = {
     messagingSenderId: "392510000083",
     appId: "1:392510000083:web:9523decf7db6eb093efd26"
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -23,17 +22,15 @@ onAuthStateChanged(auth, (user) => {
     } else {
         const loggedInUser = user.uid;
         console.log("User logged in");
-        const docRef = doc (db, "users", loggedInUser);
+        const docRef = doc(db, "users", loggedInUser);
         getDoc(docRef)
             .then(docSnap => {
                 if (docSnap.exists()) {
                     console.log("Document data:", docSnap.data());
                     const userData = docSnap.data();
-                    document.getElementById("Enter your Username").innerText = userData.firstName;
-                    document.getElementById("Enter your ID Number").innerText = userData.email;
-                    document.getElementById("Enter your school").innerText = userData.password;
-                    document.getElementById("Enter your Email").innerText = userData.idNumber;
-                    document.getElementById("Enter your Password").innerText = userData.school;
+                    document.getElementById("loggedUserFName").innerText = userData.firstName;
+                    document.getElementById("loggedUserLName").innerText = userData.lastName;
+                    document.getElementById("loggedUserEmail").innerText = userData.email;
                 } else {
                     console.log("No such document found matching id!");
                 }
@@ -42,10 +39,10 @@ onAuthStateChanged(auth, (user) => {
                 console.log("Error getting document:", error);
             });
     }
-
 });
 
-if(logout){
+const logout = document.getElementById("logout");
+if (logout) {
     logout.addEventListener("click", function (event) {
         event.preventDefault();
         console.log("Logout button clicked");
@@ -63,11 +60,7 @@ if(logout){
     console.log("Logout button not found");
 }
 
-const clearToken =(idToken) => {
+const clearToken = (idToken) => {
     localStorage.removeItem('idToken', idToken);
 }
-
-
-
-
 
